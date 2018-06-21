6ce61b4306d15f5b756d85e825af45168cbf588b
Rajat <rajatbhatta6399@gmail.com>
2018-06-21 22:45:10 +0530

    add_project_post_api_added

--- Project_Based_Learning/project_modules/views.py
+++ Project_Based_Learning/project_modules/views.py
@@ -8,11 +8,16 @@ from .models import Module
 from rest_framework import status
 from .serializers import ProjectSerializer
 from .serializers import ModuleSerializer
+import json
+from .models import Project
+from .models import Module
+from teacher.models import Teacher
+from student.models import Student
 
 # Create your views here.
 
 #List all teachers or create a new teacher
-#teachers/
+#teachers
 class ProjectList(APIView):
 
 	def get(self, request):
@@ -20,8 +25,40 @@ class ProjectList(APIView):
 		serializer = ProjectSerializer(projects, many=True)
 		return Response(serializer.data)
 
-	def post(self):
-		pass
+	def post(self, request, format='json'):
+		user = request.user
+		if user is not None:
+			if user.is_active:
+				teacher_user = Teacher.objects.filter(user=user)
+				if len(teacher_user):
+					data = request.data
+					project = Project(project_name=data['project_name'], description=data['description'], number_of_modules=data['number_of_modules']);
+					project.save()
+					modules = data['modules']
+					for i in modules:
+						module = Module(module_name=i['module_name'], description=i['description'])
+						module.save()
+						project.modules.add(module)
+
+					project.save()
+					teacher_user[0].projects.add(project)
+					teacher_user[0].save()
+					js = json.dumps({"status" : "true", "msg" : "Project Successfully Added"})
+					return Response(js, status=status.HTTP_201_CREATED)
+				else:
+					js = json.dumps({"status" : "false", "msg" : "Not a valid Student"})
+					return Response(js, status=status.HTTP_400_BAD_REQUEST)
+			else:
+				js = json.dumps({"status" : "false", "reason" : "You need to activate your account. Please check your email"})
+				return Response(js, status=status.HTTP_400_BAD_REQUEST)
+		else:
+				js = json.dumps({"status" : "false", "reason" : "Please login and try again"})
+				return Response(js, status=status.HTTP_400_BAD_REQUEST)
+		
+
+		print(user.username)
+		print(request.data)
+		
 
 class ProjectInfo(generics.ListAPIView):
 	serializer_class = ProjectSerializer
