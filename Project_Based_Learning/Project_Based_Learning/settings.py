8126f362d146af9dab9c83b6a62bea218b5a7208
Rajat <rajatbhatta6399@gmail.com>
2018-07-02 16:33:44 +0530

    docker_work_done

--- Project_Based_Learning/Project_Based_Learning/settings.py
+++ Project_Based_Learning/Project_Based_Learning/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i+zl+0c@-@1w^bp_)ecl9-bbux1q=stwxxf43ker%e%0b)%zd#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 
